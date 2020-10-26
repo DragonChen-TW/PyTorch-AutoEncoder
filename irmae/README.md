0b1f0122364933b589201340666ce3791cca59f2
teacher144123 <teacher144123@gmail.com>
2020-10-27 02:10:15 +0800

    add plots and check.ipynb

--- irmae/README.md
+++ irmae/README.md
@@ -1,9 +1,33 @@
 # Implicit Rank-Minimizing Autoencoder
 
-This is implementation of this paper
-*Implicit Rank-Minimizing Autoencoder*
+This is implementation of this paper  
+*Implicit Rank-Minimizing Autoencoder*  
 https://arxiv.org/abs/2010.00679
 
 ## run yourself
-run `python3 train.py`
+1. train model
+run `python3 train.py` to train model checkpoint file into `./ckpts`  
+If you wanna see comparison of vanilla model  
+run ../ae/train.py also
+2. inspect the effect
+run `check.ipynb` to inspect the effect of irmae  
 
+## Plot
+### Interpolutions
+
+IRMAE I = 4, letent_dim = 128
+
+IRMAE
+![irmae1](./plots/irmae1.png)
+Vanilla AE
+![ae1](.plots/ae1.png)
+
+IRMAE
+![irmae2](./plots/irmae2.png)
+Vanilla AE
+![ae2](.plots/ae2.png)
+
+IRMAE
+![irmae2](./plots/irmae2.png)
+Vanilla AE
+![ae2](.plots/ae2.png)
--- irmae/model.py
+++ irmae/model.py
@@ -9,6 +9,46 @@ class Unflatten(nn.Module):
     
     def __call__(self, x):
         return x.view(-1, *self.dims)
+
+class ConvAutoEncoder(nn.Module):
+    # 1.57 M
+    def __init__(self, latent_dim=128):
+        super().__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 24, kernel_size=4, stride=2, padding=1), # 24, 14, 14
+            nn.ReLU(True),
+            nn.Conv2d(24, 48, kernel_size=4, stride=2, padding=1), # 48, 7, 7
+            nn.ReLU(True),
+            nn.Conv2d(48, 24, kernel_size=1), # 24, 7, 7
+            nn.ReLU(True),
+            nn.Flatten(),
+            nn.Linear(24 * 7 * 7, latent_dim),
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, 24 * 7 * 7),
+            Unflatten(24, 7, 7),
+            nn.ConvTranspose2d(24, 48, kernel_size=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(48, 24, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(24, 1, kernel_size=4, stride=2, padding=1),
+            nn.Tanh(),
+        )
+
+    def forward(self, x):
+        out = self.encoder(x)
+        out = self.decoder(out)
+
+        return out
+
+class Unflatten(nn.Module):
+    def __init__(self, c, h, w):
+        super().__init__()
+        self.dims = (c, h, w)
+    
+    def __call__(self, x):
+        return x.view(-1, *self.dims)
 class IRMAE(nn.Module):
     # 1.57 M
     def __init__(self, len_w=4, latent_dim=128, mode='normal'):
